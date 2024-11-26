6aecaa6aa1af143f0dc72d2dd756686abdbdd373
selemoi <koppus.victor@strathmore.edu>
2024-11-26 19:12:13 +0300

    Create file main.cpp

---
+++ file main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//Declare an external variable
+extern int globalVar=100;
+
+int main()
+{
+    //Access the external variable
+    cout << "In main, globalVar: " << globalVar << endl;
+    //Modify the external variable
+    globalVar = 200;
+    cout << "In main after modification, globalVar:" <<globalVar <<endl;
+    return 0;
+}
