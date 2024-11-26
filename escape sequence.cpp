53cb8269a2eb4acb8ef7c386374ec8ea6b05a8b9
selemoi <koppus.victor@strathmore.edu>
2024-11-26 19:11:04 +0300

    Create escape sequence.cpp

---
+++ escape sequence.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"\t Escape Sequence Practice \n";
+    cout <<"BBIT is a university diploma designed to focus on:";
+    cout <<"\t 1.Development \n";
+    cout <<"\t 2.Implementation, and \n";
+    cout <<"t 3. Management of Information Systems \n";
+    return 0; 
+}
